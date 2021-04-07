1a602eb80e1a473babce91a1b707373cd751e87b
Denys Daniielian <67520699+d3n1x1@users.noreply.github.com>
2021-04-08 02:18:20 +0300

    Updates

---
+++ static/js/main.aa18468a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(e,t,o){"use strict";o.r(t);var n=o(1),c=o.n(n),r=o(3),i=o.n(r),h=(o(8),o(0));var s=function(){return Object(h.jsx)("h1",{children:"Hello world"})};i.a.render(Object(h.jsx)(c.a.StrictMode,{children:Object(h.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,o){}},[[10,1,2]]]);
+//# sourceMappingURL=main.aa18468a.chunk.js.map
